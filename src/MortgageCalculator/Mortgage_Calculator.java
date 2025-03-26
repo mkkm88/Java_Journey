058a515e276e8e7be56dc8580d8cb016b54eb28a
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-26 14:26:25 +0530

    Changes in Mortgage Calculator

--- src/MortgageCalculator/Mortgage_Calculator.java
+++ src/MortgageCalculator/Mortgage_Calculator.java
@@ -6,26 +6,28 @@ import java.util.Scanner;
 
 public class Mortgage_Calculator {
     public static void main(String[] args) {
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        // Taking Input From User
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Principal : ");
         int principal =  scanner.nextInt();
 
         System.out.print("Annual Interest Rate : ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate/12/100;
-
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;          // Calculating Monthly Interest
 
         System.out.print("Period (Years) : ");
-        byte period = scanner.nextByte();
-        int numberOfPayments = period*12;
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;                  // Calculating Number of Payments
 
+        double powerOfNumbers = Math.pow((1 + monthlyInterest), numberOfPayments);    // Calculating (1+r)power(n)
 
-        double powerOfNumbers = Math.pow((1 + monthlyInterestRate), numberOfPayments);
-
-
-        double mortgage = principal * ((monthlyInterestRate*powerOfNumbers)/(powerOfNumbers-1));
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        double mortgage = principal * ((monthlyInterest*powerOfNumbers)/(powerOfNumbers-1));  // Calculating Mortgage
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);    // Formating Mortgage into Currency
         System.out.print("Mortgage : " + mortgageFormatted);
     }
 
