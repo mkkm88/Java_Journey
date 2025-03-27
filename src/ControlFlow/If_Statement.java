4c9eb682b471d0955bd219b7f6626ea11e733c90
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-03-27 13:05:11 +0530

    If statement code is added

---
+++ src/ControlFlow/If_Statement.java
@@ -0,0 +1,19 @@
+package ControlFlow;
+
+import java.util.Scanner;
+
+public class If_Statement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Temperature : ");
+        int temperature = scanner.nextInt();
+
+        if (temperature > 30)
+            System.out.println("It's a hot day! Drink plenty of water");
+        else if (temperature >= 20)
+            System.out.println("It's a nice day");
+        else
+            System.out.println("It's cold");
+    }
+}
