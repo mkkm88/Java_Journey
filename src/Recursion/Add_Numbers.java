661428e6aad7e0b250ab08e52cc46386e16e3a4f
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-04-06 12:24:46 +0530

    Recursion example added

---
+++ src/Recursion/Add_Numbers.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class Add_Numbers {
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+    }
+    public static int sum(int k){
+        if(k > 0)
+            return k + sum(k - 1);
+        else
+            return 0;
+    }
+}
