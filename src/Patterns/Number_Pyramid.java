e5bfd9495f2fbbcecf5ec44eb9422c0c6cb7d162
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-04-09 00:04:47 +0530

    Solid Rhombus Modified

---
+++ src/Patterns/Number_Pyramid.java
@@ -0,0 +1,8 @@
+package Patterns;
+
+public class Number_Pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+
+    }
+}
--- src/Patterns/Solid_Rhombus.java
+++ src/Patterns/Solid_Rhombus.java
@@ -16,10 +16,10 @@ public class Solid_Rhombus {
                 System.out.print("*");
             }
 
-            // inner loop -> space print
+            /*// inner loop -> space print
             for (int j = 1; j<=i-1; j++){
                 System.out.print(" ");
-            }
+            }*/
             System.out.println();
         }
 
