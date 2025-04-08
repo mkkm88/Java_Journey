fe9e4721d8b6eafa8e2dfc46d3af961c38421d6c
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-04-09 00:17:29 +0530

    Modified Numbers full pyramid

--- src/Patterns/Number_Pyramid.java
+++ src/Patterns/Number_Pyramid.java
@@ -13,8 +13,7 @@ public class Number_Pyramid {
 
             // inner loop -> print number
             for (int j = 1; j <= i; j++){
-                System.out.print(i);
-                System.out.print(" ");
+                System.out.print(i+" ");
             }
             System.out.println();
         }
