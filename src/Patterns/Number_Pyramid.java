91adf211f6d46c6abe8bd606b27f205d055404f0
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-04-09 00:16:22 +0530

    Numbers Full Pyramid

--- src/Patterns/Number_Pyramid.java
+++ src/Patterns/Number_Pyramid.java
@@ -4,5 +4,19 @@ public class Number_Pyramid {
     public static void main(String[] args) {
         int n = 5;
 
+        //outer loop
+        for (int i = 1; i<=n; i++) {
+            //inner loop -> print space
+            for (int j =1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            // inner loop -> print number
+            for (int j = 1; j <= i; j++){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
