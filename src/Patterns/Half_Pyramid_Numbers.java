9b5a8465995042b9eac4668f31d6f308caa974e0
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-04-08 23:21:10 +0530

    Pyramids with numbers

---
+++ src/Patterns/Half_Pyramid_Numbers.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Half_Pyramid_Numbers {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Patterns/Inverted_Half_Pyramid_Numbers.java
@@ -0,0 +1,27 @@
+package Patterns;
+
+public class Inverted_Half_Pyramid_Numbers {
+    public static void main(String[] args) {
+        int n = 5;
+
+        /*// outer loop
+        for (int i = n; i >= 1; i--) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }*/
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= n-i+1; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
