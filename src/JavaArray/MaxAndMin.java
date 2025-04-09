b1501037cd6e632ec7ffaca9d33d3292c2419fd9
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-04-09 22:11:10 +0530

    Maximum and minimum of an array of integers

--- src/JavaArray/MaxAndMin.java
+++ src/JavaArray/MaxAndMin.java
@@ -1,7 +1,30 @@
 package JavaArray;
 
+import java.util.Scanner;
+
 public class MaxAndMin {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] numbers = new int[size];
 
+        //input
+        for(int i=0; i<size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+            if (number > max) {
+                max = number;
+            }
+        }
+        System.out.println("Largest number is : " + max);
+        System.out.println("Smallest number is : " + min);
     }
 }
